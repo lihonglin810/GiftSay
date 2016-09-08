d4d8599cbbf3c99f7c17156d1fbcd57d89e5b53d
lihonglin <563418365@qq.com>
2016-09-08 19:29:07 +0800

    构建框架

--- GiftSay/app/src/main/java/com/lanou3g/dllo/giftsay/ui/activity/MainActivity.java
+++ GiftSay/app/src/main/java/com/lanou3g/dllo/giftsay/ui/activity/MainActivity.java
@@ -1,7 +1,10 @@
-package com.lanou3g.dllo.giftsay;
+package com.lanou3g.dllo.giftsay.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+
+import com.lanou3g.dllo.giftsay.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
