868dbb7f56615e9097a279e97f6d981b6f2dff44
lihonglin <563418365@qq.com>
2016-09-10 16:09:49 +0800

    4个Fragment的布局基本完成

---
+++ GiftSay/app/src/main/java/com/lanou3g/dllo/giftsay/ui/adapter/CategoryAdapter.java
@@ -0,0 +1,28 @@
+package com.lanou3g.dllo.giftsay.ui.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by dllo on 16/9/10.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter{
+    private List<Fragment> fragments;
+    public CategoryAdapter(FragmentManager fm,List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
--- GiftSay/app/src/main/java/com/lanou3g/dllo/giftsay/ui/fragment/CategoryFragment.java
+++ GiftSay/app/src/main/java/com/lanou3g/dllo/giftsay/ui/fragment/CategoryFragment.java
@@ -1,11 +1,22 @@
 package com.lanou3g.dllo.giftsay.ui.fragment;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.widget.ListView;
+
 import com.lanou3g.dllo.giftsay.R;
+import com.lanou3g.dllo.giftsay.ui.adapter.CategoryAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dllo on 16/9/9.
  */
 public class CategoryFragment extends AbsBaseFragment{
+    private ViewPager mCategoryVp;
+    private TabLayout mCategoryTb;
     @Override
     protected int setLayout() {
         return R.layout.fragment_category;
@@ -13,11 +24,24 @@ public class CategoryFragment extends AbsBaseFragment{
 
     @Override
     protected void initViews() {
-
+        mCategoryVp = byView(R.id.category_vp);
+        mCategoryTb = byView(R.id.category_tb);
     }
 
     @Override
     protected void initDatas() {
+        CategoryPageTab();
+    }
 
+    private void CategoryPageTab() {
+        List<Fragment> datas = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            datas.add(new HomeCommonFragment());
+        }
+        CategoryAdapter categoryAdapter = new CategoryAdapter(getChildFragmentManager(),datas);
+        mCategoryVp.setAdapter(categoryAdapter);
+        mCategoryTb.setupWithViewPager(mCategoryVp);
+        mCategoryTb.getTabAt(0).setText("攻略");
+        mCategoryTb.getTabAt(1).setText("单品");
     }
 }
